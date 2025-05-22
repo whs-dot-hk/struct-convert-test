375cb92c571dab2b2da03209fcd67b8feb615cb7
whs <hswongac@gmail.com>
2025-05-22 02:05:30 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,35 @@ use struct_convert::Convert;
 #[derive(Convert)]
 #[convert(into = "B")]
 struct A {
-    value: i64,
+    value: C,
 }
 
 struct B {
-    value: i64,
+    value: D,
+}
+
+enum C {
+    A,
+    B,
+}
+
+#[derive(Debug)]
+enum D {
+    A,
+    B,
+}
+
+impl From<C> for D {
+    fn from(c: C) -> Self {
+        match c {
+            C::A => D::A,
+            C::B => D::B,
+        }
+    }
 }
 
 fn main() {
-    let a = A { value: 1 };
+    let a = A { value: C::A };
     let b: B = a.into();
-    println!("{}", b.value);
+    println!("{:?}", b.value);
 }
